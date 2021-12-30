f597572a1a615588ad618adea46ff82ecee42903
shashankDEV21 <shashankchaturvedi68@gmail.com>
2021-12-31 00:51:56 +0530

    socket commit

---
+++ chatpub/client.js
@@ -0,0 +1,36 @@
+let socket=io()
+
+$(document).ready(function(){
+$('#loginbox').show()
+$('#chatbox').hide()
+$('#btnuname').click(()=>{
+socket.emit('login',{
+    username:$('#uname').val(),
+    password:$('#pass').val()
+})
+})
+socket.on('loggedin',()=>{
+    $('#loginbox').hide()
+    $('#chatbox').show()
+    
+})
+$('#btnsend').click(()=>{
+    socket.emit('msgsend',{
+        to:$('#touser').val(),
+        msg:$('#inpmsg').val(),
+        from:$('#uname').val()
+    })
+})
+socket.on('msgrcved',(data)=>{
+// let lim=document.createElement('li')
+// lim.innerText=data.msg
+
+//$('#ulmsg').append($('<li>').text(data.from+' -'+data.msg)) 
+$('#ulmsg').append($('<li>').text(`[${data.from}] : ${data.msg}` ))
+})
+socket.on('fail',()=>{
+    window.alert('Username or Password is incorrect')
+})
+
+    
+})
---
+++ chatserver.js
@@ -0,0 +1,56 @@
+const express=require('express')
+const app=express()
+const http=require('http')
+const socketio=require('socket.io')
+const { Socket } = require('dgram')
+const server=http.createServer(app)
+const io=socketio(server)
+
+ app.use('/',express.static(__dirname+'/chatpub'))
+
+let users={        //let a={} it means a is an object
+shashank:'12345',
+}
+
+io.on('connection',(socket)=>{
+    console.log(socket.id)
+// socket.on('sendmsg',(data)=>{
+//     io.emit('msgrcv',data)
+    
+// })
+function log(s,u){
+    s.join(u)
+    s.emit('loggedin')
+    // socketMap[s.id]=u
+    // console.log(socketMap)
+}
+socket.on('login',(data)=>{
+    if(users[data.username]){
+        if(users[data.username]==data.password){
+            // socket.join(data.username)
+            // socket.emit('loggedin')    
+            log(socket,data.username)
+        }else{
+            socket.emit('fail')
+        }
+
+    }else{
+        users[data.username]=data.password
+        // socket.join(data.username)
+        // socket.emit('loggedin')
+        log(socket,data.username)
+    }
+  
+})
+socket.on('msgsend',(data)=>{
+    //data.from=socketMap[socket.id]
+    if(data.to){
+        io.to(data.to).emit('msgrcved',data)
+    }else{
+        socket.broadcast.emit('msgrcved',data)
+    }
+})
+})
+server.listen(4444,()=>{
+console.log('server listening at http://localhost:4444')
+})
